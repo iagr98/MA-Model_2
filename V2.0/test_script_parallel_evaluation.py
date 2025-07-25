56e489aebd9322f92e7c8944e84e13bb636b6c6b
iagr98 <ivan.rivillas@correounivalle.edu.co>
2025-07-25 17:59:37 +0200

    Updated parallel code for running detail lambda tests. Also added detail_lambda sheet to settings folder

--- V2.0/test_script_parallel_evaluation.py
+++ V2.0/test_script_parallel_evaluation.py
@@ -6,7 +6,7 @@ from test_script import run_sim
 
 N_CPU = 8
 
-experiment = "detail_V_dis" # "main" for ye + niba experiements, "sozh" for experiments from AVT.FVT lab.
+experiment = "detail_lambda" # "main" for ye + niba experiements, "sozh" for experiments from AVT.FVT lab.
 
 df = pd.read_excel("settings/data_main.xlsx", sheet_name=experiment)
 exp = df['exp'].tolist()
@@ -19,23 +19,23 @@ if (experiment == "sozh" or experiment == "detail_V_dis"):
 
 
 def parallel_simulation(params):
-    if (experiment == "main"):
+    if (experiment == "main" or experiment == "detail_lambda"):
         exp, phi_0, dV_ges, eps_0 = params
     elif (experiment == "sozh" or experiment == "detail_V_dis"):
         exp, phi_0, dV_ges, eps_0, h_dp_max, h_w_0 = params
     print(f"Start simulation with exp={exp}, phi_0={phi_0}, dV_ges={dV_ges}, eps_0={eps_0}")
     try:
-        if (experiment == "main"):
+        if (experiment == "main" or experiment == "detail_lambda"):
             Sim = run_sim(exp, phi_0, dV_ges, eps_0)
             return {'exp': exp, 'phi_0': phi_0, 'dV_ges': dV_ges, 'eps_0': eps_0,
-                    'V_dp': Sim.V_dp, 'Sep. Eff.': Sim.E, 'status': 'success'}
+                    'V_dp': Sim.V_dp, 'Sep. Eff.': Sim.E,'h_dpz': Sim.h_dpz ,'L_dp': Sim.L_dp , 'status': 'success'}
         elif (experiment == "sozh" or experiment == "detail_V_dis"):
             Sim = run_sim(exp, phi_0, dV_ges, eps_0, h_dp_max, h_w_0)
             return {'exp': exp, 'phi_0': phi_0, 'dV_ges': dV_ges, 'eps_0': eps_0,
                     'h_dp_max': h_dp_max, 'h_w_0': h_w_0,
                     'V_dp': Sim.V_dp, 'Sep. Eff.': Sim.E,'h_dpz': Sim.h_dpz ,'L_dp': Sim.L_dp , 'status': 'success'}
     except Exception as e:
-        if (experiment == "main"):
+        if (experiment == "main" or experiment == "detail_lambda"):
             print(f"Simulation failed for exp={exp}, phi_0={phi_0}, dV_ges={dV_ges}, eps_0={eps_0}: {str(e)}")
             return {'exp': exp, 'phi_0': phi_0, 'dV_ges': dV_ges, 'eps_0': eps_0, 'error': str(e), 'status': 'failed'}
         elif (experiment == "sozh" or experiment == "detail_V_dis"):
@@ -45,7 +45,7 @@ def parallel_simulation(params):
 
 if __name__ == "__main__":
     
-    if (experiment == "main"):
+    if (experiment == "main" or experiment == "detail_lambda"):
         parameters = [(exp[i], phi_0[i], dV_ges[i], eps_0[i]) for i in range(len(exp))]
     elif (experiment == "sozh" or experiment == "detail_V_dis"):
         parameters = [(exp[i], phi_0[i], dV_ges[i], eps_0[i], h_dp_max[i], h_w_0[i]) for i in range(len(exp))]
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     h_dpz_columns.columns = [f'h_dpz_{i}' for i in range(h_dpz_columns.shape[1])]
     df_results = df_results.drop(columns=['h_dpz'])
     df_results = pd.concat([df_results, h_dpz_columns], axis=1)  # Concatenate V_dis columns with the main result dataframe
-    df_results.to_csv('simulation_results_parallel_evaluation_detail.csv', index=False)
+    df_results.to_csv('simulation_results_parallel_evaluation_detail_lambda.csv', index=False)
     print("Alle Simulationen abgeschlossen. Ergebnisse gespeichert.")
 
    
